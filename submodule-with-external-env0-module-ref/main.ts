8222d6371b6a7ef6c156938778f0d0f9e34c46c5
ronnyorot <110377153+ronnyorot@users.noreply.github.com>
2022-12-21 16:05:24 +0200

    Create  submodule-with-external-env0-module-ref

---
+++ submodule-with-external-env0-module-ref/main.ts
@@ -0,0 +1,4 @@
+module "sub-module-with-reference-to-internal-module" {
+  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/sub-module-with-reference-to-internal-module/ronnys"
+  version = "0.0.2"
+}
