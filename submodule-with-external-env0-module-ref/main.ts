59ce93fee194e76cc14041293ddef7520bd3904e
ronnyorot <110377153+ronnyorot@users.noreply.github.com>
2022-12-21 16:36:21 +0200

    Update main.ts

--- submodule-with-external-env0-module-ref/main.ts
+++ submodule-with-external-env0-module-ref/main.ts
@@ -1,4 +1,4 @@
 module "sub-module-with-reference-to-external-env0-module" {
-  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/sub-module-with-reference-to-internal-module/ronnys"
-  version = "0.0.2"
+  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/submodule/ronny"
+  version = "0.0.4"
 }
